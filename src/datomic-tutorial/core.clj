bad6dd28aca5aeffa5dcdf43049937e0544c506b
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2020-04-22 15:50:42 +0200

    Play around with update and history principles

--- src/datomic-tutorial/core.clj
+++ src/datomic-tutorial/core.clj
@@ -1,6 +1,7 @@
 (ns datomic-tutorial.core
   (:require [datomic.client.api :as d]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [clojure.pprint :as pp]))
 
 (def client (-> "aws-config.edn"
                 slurp
@@ -20,16 +21,26 @@
 
              {:db/ident       :todo/description
               :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one}
+
+             {:db/ident       :todo/gid
+              :db/valueType   :db.type/string
               :db/cardinality :db.cardinality/one}])
 
 (d/transact conn {:tx-data schema})
 
-(d/transact conn {:tx-data [{:list/title "learn datomic"
-                             :list/todo [{:todo/description "learn how to create a database"}
-                                         {:todo/description "learn how to add the schema"}]}]})
+(def first-id (str (java.util.UUID/randomUUID)))
+(def second-id (str (java.util.UUID/randomUUID)))
+(d/transact conn {:tx-data
+                  [{:list/title "learn datomic"
+                    :list/todo [{:todo/gid first-id
+                                 :todo/description "learn how to create a database"}
+                                {:todo/gid second-id
+                                 :todo/description "learn how to add the schema"}]}]})
 
-(d/q '[:find ?description
+(d/q '[:find ?gid ?description
        :where
+       [?todo :todo/gid ?gid]
        [?todo :todo/description ?description]
        [?list :list/todo ?todo]
        [?list :list/title "learn datomic"]]
@@ -45,3 +56,29 @@
        :where
        [?list :list/title "learn datomic"]]
      (d/db conn))
+
+
+;; **Updating entity**
+
+(def db-id (ffirst (d/q '[:find ?todo
+                          :in $ ?gid
+                          :where
+                          [?todo :todo/gid ?gid]]
+                        (d/db conn)
+                        first-id)))
+
+(d/transact conn {:tx-data [{:db/id db-id :todo/description "hello, world"}]})
+
+;; **Fetching history**
+;; https://docs.datomic.com/cloud/tutorial/history.html#orgee6614f
+
+(->> (d/q '[:find ?tx ?description ?op
+            :in $ ?gid
+            :where
+            [?todo :todo/description ?description ?tx ?op]
+            [?todo :todo/gid ?gid]]
+          (d/history (d/db conn))
+          first-id)
+     (filter #(nth % 2)) ;; op at true means assertion, false retractions
+     (sort-by first)
+     (pp/pprint))
